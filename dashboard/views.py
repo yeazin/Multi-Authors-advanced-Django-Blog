7f13f82c84714a31d39f910bf9296083b4c607af
yeazin <farsin310yeariha9701@gmail.com>
2021-04-18 17:11:47 +0600

    added post stat

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,14 +8,18 @@ class Dashboard(View):
         user = request.user 
         post = user.author.blog_set.all()
         post_count = post.count()
-        post_active = user.author.blog_set.filter(status='active').count()
-        post_pending = user.author.blog_set.filter(status='pending').count()
+        post_active = user.author.blog_set.filter(status='active')
+        post_active_count = post_active.count()
+        post_pending = user.author.blog_set.filter(status='pending')
+        post_pending_count = post_pending.count()
         context= {
             'user':user,
             'post':post,
             'post_count':post_count,
             'post_active':post_active,
-            'post_pending':post_pending
+            'post_pending':post_pending,
+            'post_active_count':post_active_count,
+            'post_pending_count':post_pending_count
 
         }
         return render(request,'dashboard/dashboard.html',context)
